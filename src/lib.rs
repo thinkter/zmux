d230c4b6a58dc5f32225983a85b6846848817a2e
thinkter <ashmangamer0406@gmail.com>
2026-07-11 15:25:09 +0530

    style: format public exports

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,7 @@ mod theme;
 mod welcome;
 mod workspaces;
 
-pub use app::{
-    JumpToLatestNotification, NotifyCurrentPane, init_zmux, open_zmux_workspace, run,
-};
+pub use app::{JumpToLatestNotification, NotifyCurrentPane, init_zmux, open_zmux_workspace, run};
 pub use cli_server::{CliNotification, CliServer, NOTIFICATION_ENDPOINT_ENV, NotificationEndpoint};
 pub use env::terminal_env;
 pub use keymap::{NewTerminal, configure_keybindings, configure_zoom_actions};
